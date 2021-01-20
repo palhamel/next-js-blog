a19f4b4c80c7e16b9b8be0d175a74a02d5948717
Pål Hamel <pal.hamel@gmail.com>
2021-01-20 16:25:46 +0100

    add more css

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,10 @@
-// css:
+import Head from 'next/head'
 import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
+const name = 'Vingummiguiden'
+export const siteTitle = 'Magisk bra info om vingummin'
 
 export default function Layout({ children }) {
   return <div className={styles.container}>{children}</div>
