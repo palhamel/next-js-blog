bfa7d22ddf6510b995a04e39b650ec8aef0eaea1
Pål Hamel <pal.hamel@gmail.com>
2021-01-20 16:58:24 +0100

    setup posts and install for parse markdown

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,10 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>"Smaken av ett gott vingummi är som.."</p>
-        <p>
+        {/* <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        </p> */}
       </section>
     </Layout>
   )
